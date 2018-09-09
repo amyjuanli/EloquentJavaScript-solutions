8a5bbbf49884bcaa706b184699245c763a71b86b
juan-ONE <40164608+juan-ONE@users.noreply.github.com>
2018-09-09 16:21:28 +0200

    create nth() function in regular way

--- 04_dataStruture.js
+++ 04_dataStruture.js
@@ -3,25 +3,25 @@
 */
 function range(start, end, step = 1) {
     let arr = [];
-    
+
     if (start < end) {
         for (let i = start; i <= end; i += step) {
             arr.push(i);
-        } 
+        }
     } else {
         for (let i = start; i >= end; i += step) {
             arr.push(i);
         }
-    } 
-  return arr;
+    }
+    return arr;
 }
 
 function sum(arr) {
-	let sum = 0;
-  	for(let num of arr) {
-    	sum += num;
+    let sum = 0;
+    for (let num of arr) {
+        sum += num;
     }
-  return sum;
+    return sum;
 }
 console.log(range(1, 10));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -43,27 +43,27 @@ console.log(sum(range(1, 10)));
 */
 function reverseArray(arr) {
     let newArr = [];
-    for (let i = arr.length-1; i >= 0; i--) {
+    for (let i = arr.length - 1; i >= 0; i--) {
         newArr.push(arr[i]);
     }
-   return newArr;
-  }
-  console.log(reverseArray(["A", "B", "C"]));
-  // → ["C", "B", "A"];
-  
-  function reverseArrayInPlace(arr) {
-        for (let i = 0; i < arr.length/2; i++) {
+    return newArr;
+}
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+
+function reverseArrayInPlace(arr) {
+    for (let i = 0; i < arr.length / 2; i++) {
         let old = arr[i];
-        arr[i] = arr[arr.length-1-i];
-        arr[arr.length-1-i] = old;
-      }
-    
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = old;
+    }
+
     return arr;
-  }
-  let arrayValue = [1, 2, 3, 4, 5];
-  reverseArrayInPlace(arrayValue);
-  console.log(arrayValue);
-  // → [5, 4, 3, 2, 1]
+}
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
 
 
 
@@ -81,15 +81,54 @@ function reverseArray(arr) {
 */
 function arrayToList(arr) {
     let list = null;
-      for (let i = arr.length-1; i >= 0; i--) {
-       list = {value: arr[i],
-         rest: list};
-      }
+    for (let i = arr.length - 1; i >= 0; i--) {
+        list = {
+            value: arr[i],
+            rest: list
+        };
+    }
     return list;
-  }
-  console.log(arrayToList([10, 20]));
-  // → {value: 10, rest: {value: 20, rest: null}}
+}
 
+function listToArray(list) {
+    let arr = [];
+    let pointer = list;
+    while (pointer.rest) {
+        arr.push(pointer.value);
+        pointer = pointer.rest;
+    }
+    arr.push(pointer.value);
+    return arr;
+}
+
+function prepend(element, list) {
+    return {
+        value: element,
+        rest: list
+    };
+}
+
+function nth(list, index) {
+    if (index === 0) return list.value;
+    if (!list) return undefined;
+
+    let pointer = list;
+    let n = 0; // set initial index equal zero
+    while (n < index) {
+        pointer = pointer.rest;
+        n++;
+    }
+    return pointer.value;
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
 
 
 
@@ -109,32 +148,45 @@ function arrayToList(arr) {
 */
 function deepEqual(obj1, obj2) {
     // To check whether the typeof is object
-    if ((typeof obj1 === "object") &&  
-         (typeof obj2 === "object") &&  
-          (obj1 !== null) &&
-          (obj2 !== null)) {
+    if ((typeof obj1 === "object") &&
+        (typeof obj2 === "object") &&
+        (obj1 !== null) &&
+        (obj2 !== null)) {
 
         // deep comparison
         // To check whether they have the same set of property names 
-         obj1Keys =  Object.keys(obj1);
-         obj2Keys = Object.keys(obj2);
-         if (obj1Keys.length === obj2Keys.length) {
+        obj1Keys = Object.keys(obj1);
+        obj2Keys = Object.keys(obj2);
+        if (obj1Keys.length === obj2Keys.length) {
             for (let i = 0; i < obj1Keys.length; i++) {
-                if(!obj2[obj1Keys[i]]) {
+                if (!obj2[obj1Keys[i]]) {
                     return false;
                 } else {
                     return deepEqual(obj1[obj1Keys[i]], obj2[obj1Keys[i]]);
                 }
-             }
-         }
+            }
+        }
     } else {
         return obj1 === obj2;
     }
 }
-let obj = {here: {is: "an"}, object: 2};
+let obj = {
+    here: {
+        is: "an"
+    },
+    object: 2
+};
 console.log(deepEqual(obj, obj));
 // → true
-console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {
+    here: 1,
+    object: 2
+}));
 // → false
-console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual(obj, {
+    here: {
+        is: "an"
+    },
+    object: 2
+}));
 // → true
